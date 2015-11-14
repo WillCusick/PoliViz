9cdec5c2301f6ac6daf72e5a5fefec72ccca1cd3
Will Cusick <william.m.cusick@stonybrook.edu>
2015-11-14 13:39:34 -0500

    Fixed a non-JSON-Serializable error.
    Re-"boxed" the Tweepy Place and BoundingBox Models because their _api attr
    made them unserializable.

--- receiver.py
+++ receiver.py
@@ -8,12 +8,30 @@ import pika
 class PoliStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
+        # Re-box the place and bounding box information because
+        # Tweepy's models are not JSON-serializable
+        if status.place is not None:
+            if status.place.bounding_box is not None:
+                bounding_box = {'coordinates':
+                                status.place.bounding_box.coordinates}
+            else:
+                bounding_box = None
+        
+            place = {'country_code': status.place.country_code,
+                     'url': status.place.url,
+                     'place_type': status.place.place_type,
+                     'bounding_box': bounding_box,
+                     'full_name': status.place.full_name,
+                     'name': status.place.name}
+        else:
+            place = None
+        
         message = {'author_name': status.author.screen_name,
                    'author_id': status.author.id,
                    'id': status.id,
                    'text': status.text,
                    'coordinates': status.coordinates,
-                   'place': status.place,
+                   'place': place,
                    'time': int(time.time())}
         self.callback(json.dumps(message))
             
@@ -51,10 +69,13 @@ class PoliReceiver(object):
                 time.sleep(10)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Receive Twitter Stream API events')
-    parser.add_argument('-c', '--conf', nargs='?', default='twit_api.conf',
+    parser = argparse.ArgumentParser(
+        description='Receive Twitter Stream API events')
+    parser.add_argument('-c', '--conf', nargs='?',
+                        default='twit_api.conf',
                         help='location of config file containing API keys')
-    parser.add_argument('hashtag', nargs='+', help='list of hashtags to track')
+    parser.add_argument('hashtag', nargs='+',
+                        help='list of hashtags to track')
     args = vars(parser.parse_args())
     print args
     
@@ -65,12 +86,14 @@ if __name__ == "__main__":
             conf_option_name = line[:sep]
             config[conf_option_name] = line[sep+1:].rstrip()
 
-    rmq_connection = pika.BlockingConnection(pika.ConnectionParameters(
-        'localhost'))
+    rmq_connection = pika.BlockingConnection(
+        pika.ConnectionParameters('localhost'))
     rmq_producer = PoliProducer(rmq_connection)
 
     receiver = PoliReceiver()
-    auth = tweepy.OAuthHandler(config['consumer_key'],config['consumer_secret'])
-    auth.set_access_token(config['access_token_key'], config['access_token_secret'])
+    auth = tweepy.OAuthHandler(config['consumer_key'],
+                               config['consumer_secret'])
+    auth.set_access_token(config['access_token_key'],
+                          config['access_token_secret'])
     receiver.setup(auth, rmq_producer.produce)
     receiver.stream_filter(args['hashtag'])
