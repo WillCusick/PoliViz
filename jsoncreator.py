c869c1a7da022676544768be05192b918e0051be
Will Cusick <william.m.cusick@stonybrook.edu>
2015-11-14 19:46:50 -0500

    Improved pruning algorithm.
    Now prunes tweets when more than twice the max load is stored

--- jsoncreator.py
+++ jsoncreator.py
@@ -6,8 +6,22 @@ max_tweets_per_load = 200
 delimit = ":\\:"
 len_delimit = len(delimit)
 
+def prune(red, tweet):
+    breaking_point = tweet.find(":\\:")
+        
+    time_added = datetime.strptime(tweet[breaking_point+len_delimit:], "%Y-%m-%d %H:%M:%S.%f")
+    time_now = datetime.now()
+    time_elapsed = time_now - time_added
+    if time_elapsed.total_seconds() > 120:
+        red.srem("tweets", tweet)
+        return True
+    return False
+
 def geoFormat(red):
-    tweets = red.smembers("tweets")
+    if red.scard("tweets") > 2*max_tweets_per_load:
+        for tweet in red.smembers("tweets"):
+            prune(red, tweet)
+    
     json_dict = {'type': 'FeatureCollection',
                  'crs': {
                      'type': 'name',
@@ -18,19 +32,14 @@ def geoFormat(red):
                  'features': []
     }
     tweet_count = 0
-    for tweet in tweets:
+    for tweet in red.smembers("tweets"):
         if tweet_count >= max_tweets_per_load:
             break
         
-        breaking_point = tweet.find(":\\:")
-        
-        time_added = datetime.strptime(tweet[breaking_point+len_delimit:], "%Y-%m-%d %H:%M:%S.%f")
-        time_now = datetime.now()
-        time_elapsed = time_now - time_added
-        if time_elapsed.total_seconds() > 120:
-            red.srem("tweets", tweet)
+        if prune(tweet):
             continue
         
+        breaking_point = tweet.find(":\\:")
         tweet_json = {'type': 'Feature'}
         tweet_json['geometry'] = json.loads(tweet[:breaking_point].replace('\'','"'))
         tweet_json['properties'] = {'party': 'democrat'}
@@ -38,3 +47,5 @@ def geoFormat(red):
         tweet_count += 1
         
     return json_dict
+
+geoFormat(redis.StrictRedis(host='localhost', port=6379, db=0))
